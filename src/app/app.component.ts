2e3e64c52e61da4c07fc7ef8910ae88632be499e
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2025-06-23 22:52:29 +0530

    New components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, SignUpComponent,ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+@Component({
+    standalone: true,
+    selector:'app-profile',
+    template:`<h1>Profile Component</h1>`
+})
+export class ProfileComponent{
+
+}
