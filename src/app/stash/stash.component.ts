24d4bab8bf0d388f7432f58cfba6cd6da7a18aa1
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2025-07-03 22:13:30 +0530

    comp

---
+++ src/app/stash/stash.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stash',
+  standalone: true,
+  imports: [],
+  templateUrl: './stash.component.html',
+  styleUrl: './stash.component.css'
+})
+export class StashComponent {
+
+}
